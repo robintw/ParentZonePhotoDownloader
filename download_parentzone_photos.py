9888b77d48c0a1ecb347e0ee1dac1717e010003d
Aaron Whitehouse <code@whitehouse.kiwi.nz>
2021-08-22 21:20:20 +0100

    Fix login form to work, make filter work and also choose Moments as well as Observations

--- download_parentzone_photos.py
+++ download_parentzone_photos.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import time
 import os
 
@@ -26,29 +26,37 @@ def get_parentzone_photos(email, password, output_folder):
     driver.implicitly_wait(10)
 
     # Fill in email and password
-    email_field = driver.find_element_by_xpath('//*[@id="login"]/fieldset/div[1]/input')
+    email_field = driver.find_element_by_xpath('//*[@id="email"]')
     email_field.clear()
     email_field.send_keys(email)
 
-    passwd_field = driver.find_element_by_xpath('//*[@id="login"]/fieldset/div[2]/input')
+    passwd_field = driver.find_element_by_xpath('//*[@id="password"]')
     passwd_field.clear()
     passwd_field.send_keys(password)
-    passwd_field.send_keys(Keys.RETURN)
-
+    time.sleep(2)
+    # passwd_field.send_keys(Keys.RETURN)  # This isn't working for me, so click manually for now
+    login_button = driver.find_element_by_xpath("//button[@data-test-id='login_btn']")
+    login_button.click()
+    
     # Go to timeline
     driver.get('https://www.parentzone.me/#/timeline')
 
-    # Choose 'Observation' from the dropdown
-    dropdown = Select(driver.find_element_by_xpath('//*[@id="filter"]//select'))
-    dropdown.select_by_value('7')
+    # Open the filter dropdown
+    filter_button = driver.find_element_by_xpath("//button[@data-test-id='toggle_filter_button_wrapper']")
+    filter_button.click()
 
-    # Submit form
-    submit_button = driver.find_element_by_id('submit-filter')
-    submit_button.click()
+    # Choose "Observation" and "Moment", which are the two that have pictures
+    observation_filter_button = driver.find_element_by_xpath("//div[@data-test-id='tag_Observation']")
+    observation_filter_button.click()
+    moment_filter_button = driver.find_element_by_xpath("//div[@data-test-id='tag_Moment']")
+    moment_filter_button.click()
+
+    # Close filter pane
+    filter_button.click()
 
     # The page has infinite scrolling, and scrolling by the JS scroll function
     # doesn't seem to work
-    # So intead, set up a loop to scroll infinitely, and stop when we
+    # So instead, set up a loop to scroll infinitely, and stop when we
     # stop getting any more photos displaying
     html = driver.find_element_by_tag_name('html')
     old_n_photos = 0
