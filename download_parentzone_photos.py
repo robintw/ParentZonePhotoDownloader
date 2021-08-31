916ad4a32a3ab7cda537854ee1f929a6133e005c
Aaron Whitehouse <code@whitehouse.kiwi.nz>
2021-08-31 21:46:38 +0100

    Remove unnecessary import.

--- download_parentzone_photos.py
+++ download_parentzone_photos.py
@@ -6,7 +6,6 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from urllib.parse import urlparse, parse_qs
 
 
